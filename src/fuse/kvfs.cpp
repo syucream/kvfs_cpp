5aa27198cb9acc71c5959dc13da640cba37c16b1
Ryo Okubo <syucream1031@gmail.com>
2018-10-31 00:47:07 +0900

    Fill essential fuse functions

--- src/fuse/kvfs.cpp
+++ src/fuse/kvfs.cpp
@@ -1,6 +1,60 @@
+#include <string>
+#include <cerrno>
+#include <cstring>
+
 #include <fuse.h>
 
-const static struct fuse_operations kvfs_operation = {};
+using std::string;
+
+constexpr auto stat_size = sizeof(struct stat);
+
+static int kvfs_getattr(const char *path,
+                        struct stat *stbuf) {
+
+    const auto path_str = string(path);
+
+    memset(stbuf, 0, stat_size);
+
+    // TODO
+
+    return -ENOENT;
+}
+
+static int kvfs_readdir(const char *path,
+                        void *buf,
+                        fuse_fill_dir_t filler,
+                        off_t offset,
+                        struct fuse_file_info *fi) {
+
+    // TODO
+
+    return 0;
+}
+
+static int kvfs_open(const char *path,
+                     struct fuse_file_info *fi) {
+
+    // TODO
+
+    return 0;
+}
+
+static int kvfs_read(const char *path,
+                     char *buf,
+                     size_t size,
+                     off_t offset,
+                     struct fuse_file_info *fi) {
+    // TODO
+
+    return 0;
+}
+
+const static struct fuse_operations kvfs_operation = {
+        .getattr = kvfs_getattr,
+        .readdir = kvfs_readdir,
+        .open = kvfs_open,
+        .read = kvfs_read,
+};
 
 int main(int argc, char **argv) {
     struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
